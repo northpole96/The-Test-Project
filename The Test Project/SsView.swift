7dccd9f56f16444233b69e1c3d309a204b82cad3
Rajesh Khuntia <rajeshk001@icloud.com>
2023-09-05 15:10:54 +0530

    Added SsView

---
+++ The Test Project/SsView.swift
@@ -0,0 +1,18 @@
+//
+//  SsView.swift
+//  The Test Project
+//
+//  Created by Rajesh Khuntia on 05/09/23.
+//
+
+import SwiftUI
+
+struct SsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SsView()
+}
