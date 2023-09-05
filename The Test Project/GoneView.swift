6d3caace0967e93c7338be8213a6f94f431a19af
Rajesh Khuntia <rajeshk001@icloud.com>
2023-09-05 15:04:46 +0530

    add gone view

---
+++ The Test Project/GoneView.swift
@@ -0,0 +1,18 @@
+//
+//  GoneView.swift
+//  The Test Project
+//
+//  Created by Rajesh Khuntia on 05/09/23.
+//
+
+import SwiftUI
+
+struct GoneView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GoneView()
+}
