2e79bd9614596e037c476fc28548a977b4533840
Rajesh Khuntia <rajeshk001@icloud.com>
2023-10-31 18:16:18 +0530

    added

---
+++ The Test Project/TestView5.swift
@@ -0,0 +1,18 @@
+//
+//  TestView5.swift
+//  The Test Project
+//
+//  Created by Rajesh Khuntia on 31/10/23.
+//
+
+import SwiftUI
+
+struct TestView5: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView5()
+}
